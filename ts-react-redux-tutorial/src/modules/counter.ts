a7b334a27cfb8199ac3d237887f7d708ee272d93
cheonSeokHee <csh3064@naver.com>
2022-11-15 11:25:49 +0900

    veleopert react_typeScript readux 연습

--- ts-react-redux-tutorial/src/modules/counter.ts
+++ ts-react-redux-tutorial/src/modules/counter.ts
@@ -1,6 +1,14 @@
 //액션 타입 선언
 //뒤에 as const를 붙여준 이유는 나중에 액션 객체를 만들게 action.type의 값을 찾는 과정에서
 //action.type이 string으로 찾지 않고, 'counter/INCREASE'와 같이 실제 문자열 값으로 찾게 해주려고
+//상수가 아닌것을 상수인것으로 선언하는 기능
+// 즉 상수로 선언한 값이 변경되는것을 막도록 해줌 == readonly의 개념
+// const str = "hello";
+// const strConst = "hello world" as const;
+//
+// const object = { str }; // str: string;
+// const object2 = { strConst }; // strConst: 'hello world'
+
 const INCREASE = 'counter/INCREASE' as const;
 const DECREASE = 'counter/DECREASE' as const;
 const INCREASE_BY = 'counter/INCREASE_BY' as const;
